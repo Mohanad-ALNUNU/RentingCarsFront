6bb1a494e9f803361cfcd62f8ca991c778c123b4
Mohanad-ALNUNU <mohanadalnunu@gmail.com>
2023-06-08 22:30:52 +0100

    feat: add image for customers

--- src/app/customer.service.ts
+++ src/app/customer.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
-private apiURL = 'http://localhost:8081/api/customers/all';
+private apiURL = 'http://127.0.0.1:8081/api/customers/all';
   constructor(private http: HttpClient) {}
 
   getAllCustomers():Observable<any>{
